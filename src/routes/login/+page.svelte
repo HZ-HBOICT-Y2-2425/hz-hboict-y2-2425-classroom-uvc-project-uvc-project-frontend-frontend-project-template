cd4fe81523fbf777b455fa742a5a5e4efbb3247d
TijnLuckerhoff <luck0002@hz.nl>
2024-11-28 15:02:02 +0100

    created a login page that can obtain data from the backend

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,21 +1,6 @@
 <script>
-    import { onMount } from "svelte";
     import { user } from "$lib/store.js";
     let passwordIncorrect = false;
-
-    // const getData = async (url) => {
-    //     try {
-    //         const res = await fetch(url, {method: "GET"});
-    //         const data = await res.json();
-    //         return data;
-    //     } catch (err) {
-    //         throw new Error(err);
-    //     }
-    // }
-
-    onMount(async () => {
-        console.log("user", $user);
-    })
 
     async function writeToConsole() {
         const name = document.getElementById("name").value;
@@ -27,7 +12,6 @@
             data = await res.json();
             user.set(data);
             console.log("user:", $user);
-            
         } catch (err) {
             passwordIncorrect = true;
         }
