ebdd902860097bc05d36c602271829c53019e725
TijnLuckerhoff <luck0002@hz.nl>
2024-12-03 18:03:26 +0100

    Changed the fields to be saved as an object, for easier use in other pages and small functionalities

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,8 +1,13 @@
 <script>
     import { user } from "$lib/store";
 
-    let passwordIncorrect = false;
-    let emptyFields = false;
+    const inputClasses = "w-full px-4 py-2 mb-4 text-gray-700 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none";
+    let userFound = true;
+    let emptyFields = [];
+    let specificFields = {
+        user: "",
+        password: ""
+    }
     let data;
 
     if ($user) {
@@ -10,65 +15,63 @@
     }
 
     async function login() {
-        const name = document.getElementById("name").value;
-        const password = document.getElementById("password").value;
-        if (name && password) {
-            emptyFields = false;
-            const url = `http://localhost:3010/user/login?user=${name}&password=${password}`;
+        specificFields.user = document.getElementById("user").value;
+        specificFields.password = document.getElementById("password").value;
+        emptyFields = returnEmptyFields(specificFields);
+        if (emptyFields.length === 0) {
+            emptyFields = [];
+            const url = `http://localhost:3010/user/login?user=${specificFields.user}&password=${specificFields.password}`;
             try {
                 const res = await fetch(url, {method: "GET"});
                 data = await res.json();
                 user.set(data);
                 window.location.href = '/';
             } catch (err) {
-                passwordIncorrect = true;
+                userFound = false;
             }
-        } else {
-            emptyFields = true;
         }
+    }
+
+    function returnEmptyFields(object) {
+        const check = [];
+        const values = Object.values(object)
+        for (let i = 0; i < values.length; i++) {
+            if (values[i] === "") {
+                check.push(Object.keys(object)[i]);
+            }
+        }
+        return check;
     }
 </script>
 
-<!-- <div>
-    <h1>Inloggen:</h1>
-    <input id="name" placeholder="gebruikersnaam of email">
-    <input id="password" placeholder="wachtwoord">
-    {#if passwordIncorrect}
-        <p>wachwoord incorrect of gebruikersnaam incorrect</p>
-    {/if}
-    <button on:click={login}>login</button>
-</div> -->
-
-<div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-    <div class="bg-white shadow-md rounded-lg p-8 w-full max-w-sm">
+<div class="flex flex-row items-center justify-center min-h-screen bg-gray-100">
+    <section class="bg-white shadow-md rounded-lg p-8 w-full max-w-sm">
         <h1 class="text-2xl font-semibold text-gray-800 mb-6 text-center">Inloggen</h1>
         
         <input 
-            id="name" 
+            id="user" 
             placeholder="Gebruikersnaam of email" 
-            class="w-full px-4 py-2 mb-4 text-gray-700 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none" 
+            class={inputClasses}
         />
         
         <input 
             id="password" 
             type="password" 
             placeholder="Wachtwoord" 
-            class="w-full px-4 py-2 mb-4 text-gray-700 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none" 
+            class={inputClasses}
         />
-        
-        {#if passwordIncorrect}
-            <p class="text-sm text-red-500 mb-4">Wachtwoord of gebruikersnaam incorrect</p>
-        {/if}
 
-        {#if emptyFields}
+        {#if emptyFields.length > 0}
             <p class="text-sm text-red-500 mb-4">Vul alles in</p>
+        {:else if !userFound}
+            <p class="text-sm text-red-500 mb-4">Wachtwoord of gebruikersnaam incorrect</p>
         {/if}
         
         <button 
-            on:click={login} 
+            on:click={() => login()} 
             class="w-full px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
             Login
         </button>
-    </div>
+    </section>
 </div>
