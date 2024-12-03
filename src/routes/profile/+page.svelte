a51486ab40e8bb0a8c0df9a96ac96e0377082060
TijnLuckerhoff <luck0002@hz.nl>
2024-12-03 12:42:41 +0100

    made a logout button

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,11 +1,20 @@
 <script>
     import { user } from "$lib/store";
+
+    function logOut() {
+        user.set(null);
+        window.location.href = '/';
+    }
 </script>
 
 <div>
-    <p>Name: {$user.name}</p>
-    <p>Email: {$user.email}</p>
-    <p>Zipcode: {$user.zipcode}</p>
-    <p>CO2 Bijdrage: {$user.co2Contribution}</p>
-    <!-- <a href="/profile/update" class="bg-black text-white text-sm px-3 py-1">Update</a> -->
+    {#if $user}
+        <p>Name: {$user.name}</p>
+        <p>Email: {$user.email}</p>
+        <p>Zipcode: {$user.zipcode}</p>
+        <p>CO2 Bijdrage: {$user.co2Contribution}</p>
+        <!-- <a href="/profile/update" class="bg-black text-white text-sm px-3 py-1">Update</a> -->
+    {/if}
+    <button id="logOut" class="bg-black text-white text-sm px-2.5 py-1"
+    on:click={logOut}>Uitloggen</button>
 </div>
