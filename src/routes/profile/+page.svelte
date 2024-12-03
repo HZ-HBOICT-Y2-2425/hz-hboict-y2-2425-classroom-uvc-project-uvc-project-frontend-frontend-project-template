0eb2c3e93e81bbecf423f582081a2195669f47c3
TijnLuckerhoff <luck0002@hz.nl>
2024-12-03 12:27:15 +0100

    created the profile page

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { user } from "$lib/store";
+</script>
+
+<div>
+    <p>Name: {$user.name}</p>
+    <p>Email: {$user.email}</p>
+    <p>Zipcode: {$user.zipcode}</p>
+    <p>CO2 Bijdrage: {$user.co2Contribution}</p>
+    <!-- <a href="/profile/update" class="bg-black text-white text-sm px-3 py-1">Update</a> -->
+</div>
