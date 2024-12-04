f4e7b1b03ca2be99f4ec8bd6319e97babfbcdbf6
TijnLuckerhoff <luck0002@hz.nl>
2024-12-04 12:14:37 +0100

    deleted redundant functions

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,58 +1,33 @@
 <script>
     import { user } from "$lib/store";
 
-    const inputClasses = "w-full px-4 py-2 mb-4 text-gray-700 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none border border-gray-300";
     let userFound = true;
     let fieldsFilledBool = true;
     let fields = {
-        user: "",
-        password: ""
+        user: null,
+        password: null
     }
-    let data;
 
     async function login() {
         fields.user = document.getElementById("user").value;
         fields.password = document.getElementById("password").value;
-        fieldsFilledBool = areFieldsFilled(fields);
-        if (fieldsFilledBool) {
-            handleGetRequest(`http://localhost:3010/user/login?user=${fields.user}&password=${fields.password}`);
-        } else {
-            toggleBorders(fields)
-        }
-    }
-
-    function toggleBorders(object) {
-        Object.keys(object).forEach(field => {
-            let htmlField = document.getElementById(field);
-            if (htmlField.value === "") {
-                if (htmlField.classList.contains("border-gray-300")) {
-                    htmlField.classList.remove("border-gray-300");
-                    htmlField.classList.add("border-red-500");
-                }
-            } else {
-                if (htmlField.classList.contains("border-red-500")) {
-                    htmlField.classList.remove("border-red-500");
-                    htmlField.classList.add("border-gray-300");
-                }
+        if (areFieldsFilled(fields)) {
+            let url = `http://localhost:3010/user/login?user=${fields.user}&password=${fields.password}`
+            try {
+                userFound = true;
+                const res = await fetch(url, {method: "GET"});
+                let data = await res.json();
+                user.set(data);
+                window.location.href = '/';
+            } catch (err) {
+                userFound = false;
             }
-        })
-    }
-
-    async function handleGetRequest(url) {
-        try {
-            const res = await fetch(url, {method: "GET"});
-            data = await res.json();
-            user.set(data);
-            window.location.href = '/';
-        } catch (err) {
-            userFound = false;
         }
     }
 
     function areFieldsFilled(object) {
         const values = Object.values(object);
         const index = values.findIndex(value => value === "");
-        console.log("index: ", index);
         if (index === -1) {
             return true;
         }
@@ -64,11 +39,11 @@
     <section class="bg-white shadow-md rounded-lg p-8 w-full max-w-sm">
         <h1 class="text-2xl font-semibold text-gray-800 mb-6 text-center">Inloggen</h1>
         
-        <input id="user" placeholder="Gebruikersnaam of email" class={inputClasses}/>
+        <input id="user" placeholder="Gebruikersnaam of email" class="w-full px-4 py-2 mb-4 text-gray-700 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none border {fields.user === "" ? 'border-red-500' : 'border-gray-300'}"/>
         
-        <input id="password" type="password" placeholder="Wachtwoord" class={inputClasses}/>
+        <input id="password" type="password" placeholder="Wachtwoord" class="w-full px-4 py-2 mb-4 text-gray-700 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none border {fields.password === "" ? 'border-red-500' : 'border-gray-300'}"/>
 
-        {#if !fieldsFilledBool}
+        {#if !areFieldsFilled(fields)}
             <p class="text-sm text-red-500 mb-4">Vul alles in</p>
         {:else if !userFound}
             <p class="text-sm text-red-500 mb-4">Wachtwoord of gebruikersnaam incorrect</p>
