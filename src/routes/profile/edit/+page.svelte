4aed8e61b80cbbf2822ab9f0ab11c4bebdb991db
TijnLuckerhoff <luck0002@hz.nl>
2024-12-06 10:50:40 +0100

    fixed the edit profile button colours

--- src/routes/profile/edit/+page.svelte
+++ src/routes/profile/edit/+page.svelte
@@ -59,7 +59,7 @@
        
         <button 
             on:click={() => editAccount()} 
-            class="mt-6 w-full px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400"
+            class="mt-6 w-full px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
             Update Profiel
         </button>
